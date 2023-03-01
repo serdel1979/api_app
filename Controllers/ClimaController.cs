e953b89f101bd37f4486088426b6c637228f0096
unknown <sdlbsso@gmail.com>
2023-03-01 12:32:39 -0300

    falla update-database; agregué consulta al clima actual

---
+++ Controllers/ClimaController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace api_app.Controllers
+{
+
+    [ApiController]
+    [Route("clima")]
+    public class ClimaController : ControllerBase
+    {
+
+
+        public ClimaController(IConfiguration Configuration, ) {
+            this.Configuration = Configuration;
+
+            this._client = new HttpClient();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        private HttpClient _client;
+
+        [HttpGet("/lat/{lat}/longitud/{longitud}")]
+        public async Task<ActionResult> clime(string lat, string longitud)
+        {
+            try
+            {
+                var key = Configuration.GetValue<string>("ApiKey");
+                var response = await _client.GetAsync($"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={longitud}&appid={key}&lang=es");
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"latitud {lat} y {longitud} recibida");
+
+                return Ok(content);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+        }
+
+
+
+    }
+}
