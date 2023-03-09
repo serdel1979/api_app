083d0f751838761275d7f64297309e05557ca648
unknown <sdlbsso@gmail.com>
2023-03-09 12:57:25 -0300

    num d puerto y migracion

---
+++ Migrations/20230309132655_inicia-database.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_app.Migrations
+{
+    public partial class iniciadatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
